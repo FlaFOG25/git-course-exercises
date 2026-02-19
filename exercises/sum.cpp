2e38fdfe65dc5fe0ff207e1380d89c14776bf776
Flavio Micciché <miccicheflavio25@gmail.com>
2026-02-19 10:37:16 +0100

    Add sum.cpp exercise

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -6,3 +6,16 @@
   Insert the second number: 2
   Sum: 3
 */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 0;
+	cout <<"insert the first number: ";
+	cin >> a;
+	int b = 0;
+	cout <<"insert the second number: ";
+	cin >> b;
+	int c = a + b;
+	cout << "La somma di " << a << " e " << b << " è: " << c << endl; return 0;
+}
