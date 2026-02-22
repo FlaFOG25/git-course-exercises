ab18693e327c3eea6daea4097d203bb1f9b2f557
Flavio Micciché <miccicheflavio25@gmail.com>
2026-02-22 17:27:46 +0100

    fix: replace tabs with spaces in sum.cpp

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -1,21 +1,14 @@
-/*
-  Write a program that takes as input two numbers and print the sum.
-
-  Output:
-  Insert the first number: 1
-  Insert the second number: 2
-  Sum: 3
-*/
 #include <iostream>
 using namespace std;
 
 int main() {
-	int a = 0;
-	cout <<"insert the first number: ";
-	cin >> a;
-	int b = 0;
-	cout <<"insert the second number: ";
-	cin >> b;
-	int c = a + b;
-	cout << "La somma di " << a << " e " << b << " è: " << c << endl; return 0;
+int a = 0;
+cout <<"insert the first number: ";
+cin >> a;
+int b = 0;
+cout <<"insert the second number: ";
+cin >> b;
+int c = a + b;
+cout << "Sum: " << c << endl;
+return 0;
 }
