da7e4cc4c575755ba4196e69a1df47f017c54329
Flavio Micciché <miccicheflavio25@gmail.com>
2026-02-23 11:26:45 +0100

    fix: correct indentation in sum.cpp

--- exercises/sum.cpp
+++ exercises/sum.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-int a = 0;
-cout <<"insert the first number: ";
-cin >> a;
-int b = 0;
-cout <<"insert the second number: ";
-cin >> b;
-int c = a + b;
-cout << "Sum: " << c << endl;
-return 0;
+    int a = 0;
+    cout <<"insert the first number: ";
+    cin >> a;
+    int b = 0;
+    cout <<"insert the second number: ";
+    cin >> b;
+    int c = a + b;
+    cout << "Sum: " << c << endl;
+    return 0;
 }
